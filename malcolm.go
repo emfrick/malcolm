bb84452a6be27dc90530b0bfd095bd50109e0fca
Eric Frick <efrick@iq-inc.com>
2017-12-21 15:08:06 -0500

    First Commit

---
+++ malcolm.go
@@ -0,0 +1,42 @@
+package malcolm
+
+import (
+	"net/http"
+)
+
+type middleware func(http.Handler) http.Handler
+
+// Chain is a struct to store middleware
+type Chain struct {
+	middleware []middleware
+}
+
+// Then takes a middleware and returns a chain
+func (c *Chain) Then(m middleware) *Chain {
+
+	c.middleware = append([]middleware{m}, c.middleware...)
+
+	return c
+}
+
+// Create returns a handler
+func (c *Chain) Create() func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return c.Finally(h)
+	}
+}
+
+// Then takes a middleware and returns a chain
+func Then(m middleware) *Chain {
+	return &Chain{[]middleware{m}}
+}
+
+// Finally returns the entire chain
+func (c *Chain) Finally(h http.Handler) http.Handler {
+
+	for _, handler := range c.middleware {
+		h = handler(h)
+	}
+
+	return h
+}
